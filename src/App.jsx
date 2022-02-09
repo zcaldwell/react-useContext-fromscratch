d406064bb24de99003946bb8f97c296604e94894
zcaldwell <mintyst.john@gmail.com>
2022-02-08 17:50:00 -0800

    moving on to testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from './views/Home/Home';
 import { UserProvider } from './context/UserContext';
+import './App.css';
 
 export default function App() {
   return (
--- src/components/MessageList/MessageList.jsx
+++ src/components/MessageList/MessageList.jsx
@@ -3,13 +3,13 @@ import { useMessage } from '../../context/MessageContext';
 import Message from '../Message/Message';
 import styles from './MessageList.css';
 
-const { listItems } = styles;
+const { listItems, allList } = styles;
 
 export default function MessageList() {
   const { messages } = useMessage();
   return (
     <div>
-      <ul>
+      <ul className={allList}>
         {messages.map((message) => {
           return (
             <li key={`${message.name}-${message.entry}`} className={listItems}>
