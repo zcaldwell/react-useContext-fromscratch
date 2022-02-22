60f3a62acefaad201f98da9928ff501e9bdbb445
zcaldwell <mintyst.john@gmail.com>
2022-02-21 17:36:44 -0800

    context and hooks refactored with login info

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function useForm(inputs = {}) {
+  const [formState, setFormState] = useState(inputs);
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+  return { formState, handleFormChange };
+}
